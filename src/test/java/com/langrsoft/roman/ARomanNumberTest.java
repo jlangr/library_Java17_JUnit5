13c70ac3a21eb9dd392a12f5cb6d1a9d11a83eac
Gaurav <gaurav.1.agarwal@nokia.com>
2025-11-04 12:20:23 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/roman/ARomanNumberTest.java

---
+++ src/test/java/com/langrsoft/roman/ARomanNumberTest.java
@@ -0,0 +1,23 @@
+package com.langrsoft.roman;
+
+public class ARomanNumberTest {
+
+    //1 -> I
+    //5 -> V
+    //10 -> X
+    //50 -? L
+    //100 -> C
+    //500 -> D
+    //1000 -> M
+    //4,9, 40, 90, 400, 900 -> prefix testing
+    //6try with V -> 6
+    //6try with X -> 13
+    //6try with L -> 65
+    //6try with C -> 125
+    //6try with D -> 523
+    //6try with VM-> 3123
+
+
+
+
+}
