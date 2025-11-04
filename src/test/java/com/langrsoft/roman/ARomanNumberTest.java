8805860ba94f06d7088074f9ccf0dc6a08d71c66
aman-v <verma.aman@gmail.com>
2025-11-04 20:14:31 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/roman/ARomanNumberTest.java

--- src/test/java/com/langrsoft/roman/ARomanNumberTest.java
+++ src/test/java/com/langrsoft/roman/ARomanNumberTest.java
@@ -8,20 +8,51 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class ARomanNumberTest {
 
     RomanNumber romanNumber = new RomanNumber();
+    //Z -> Not supported
+
+    //O
     //1 -> I
+
+    //M
+    //2 -> II
+    //3 -> III
+
+    //B
+    //4000 -> MMMM
+
+    //Error cases
+    //<1 or >4000
+
+    //Scenarios check single symbols
     //5 -> V
     //10 -> X
     //50 -? L
     //100 -> C
     //500 -> D
     //1000 -> M
+
+    //Check combinations of symbols
+    //55 -> LV
+    //15 -> XV
+    //200 -> CC
+    //600 -> DC
+    //1500 -> MD
+
+    //Check 3 combinations of symbols
+    //1550 -> MDL
+    //210 -> CCX
+
+    //Prefix cases
     //4,9, 40, 90, 400, 900 -> prefix testing
-    //6try with V -> 6
-    //6try with X -> 13
-    //6try with L -> 65
-    //6try with C -> 125
-    //6try with D -> 523
-    //6try with VM-> 3123
+
+    //Complex cases
+    //56 -> LVI
+    //94 -> XCIV
+    //444 -> CDXLIV
+    //944 -> CMXLIV
+    //1453 -> MCDLIII
+    //3999 -> MMMCMXCIX
+
 
     @ParameterizedTest
     @CsvSource({
