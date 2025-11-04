7dde0af262fd84d122e756484fc39a7cec63ba9f
Gaurav <gaurav.1.agarwal@nokia.com>
2025-11-04 08:34:21 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/portfolio/APortfolio.java

---
+++ src/main/java/com/langrsoft/portfolio/StockPortfolio.java
@@ -0,0 +1,14 @@
+package com.langrsoft.portfolio;
+
+public class StockPortfolio {
+
+    public void purchase(String symbol, int count){
+
+    }
+
+    public int getUniqueCount(){
+        throw new UnsupportedOperationException();
+
+
+    }
+}
---
+++ src/test/java/com/langrsoft/portfolio/APortfolio.java
@@ -0,0 +1,17 @@
+package com.langrsoft.portfolio;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class APortfolio {
+
+    @BeforeEach
+    void instantiate() {
+        StockPortfolio port = new StockPortfolio();
+    }
+
+    @Test
+    public void testForZeroPurchase(){
+
+    }
+}
