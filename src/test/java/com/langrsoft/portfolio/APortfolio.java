20aa4a5a8fd536e3d550bef209a23f34d62e69e0
Sairaja Sunil <sairaj.sunil@gmail.com>
2025-11-04 08:39:43 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/portfolio/APortfolio.java

--- src/test/java/com/langrsoft/portfolio/APortfolio.java
+++ src/test/java/com/langrsoft/portfolio/APortfolio.java
@@ -1,17 +1,26 @@
 package com.langrsoft.portfolio;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class APortfolio {
 
+    StockPortfolio port;
     @BeforeEach
     void instantiate() {
-        StockPortfolio port = new StockPortfolio();
+        port = new StockPortfolio();
     }
 
     @Test
     public void testForZeroPurchase(){
+
+    }
+
+    @Test
+    public void testForSinglePurchase(){
+        port.purchase("AAPL", 10);
+        Assert.assertEquals(1, port.getUniqueCount());
 
     }
 }
