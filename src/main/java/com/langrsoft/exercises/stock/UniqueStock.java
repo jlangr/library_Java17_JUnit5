a5e5d145f0f7c2a6afeab5ce0e6f6971958a0a0a
Andre Ferreira <anFerreira@infinera.com>
2025-11-05 17:23:19 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/com/langrsoft/exercises/stock/UniqueStock.java

--- src/main/java/com/langrsoft/exercises/stock/UniqueStock.java
+++ src/main/java/com/langrsoft/exercises/stock/UniqueStock.java
@@ -4,48 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UniqueStock {
-    private final Map<String, Integer> stockHolds;
+    private final Map<String, Integer> stockHolds = new HashMap<>();
 
-    public UniqueStock() {
-        this.stockHolds = new HashMap<>();
-    }
-
-    public void purchaseStock(String symbol, int shares){
+    public void purchaseStock(String symbol, int shares) {
         String upperCase = initialValidation(symbol, shares);
-
-        int existingStock = stockHolds.getOrDefault(upperCase, 0);
-
-        if (existingStock > 0) {
-            stockHolds.put(upperCase, shares + existingStock);
-        } else {
-            stockHolds.put(upperCase, shares);
-        }
+        stockHolds.merge(upperCase, shares, Integer::sum);
     }
 
     public boolean sellStock(String symbol, int shareAmount) {
         String symbolUpperCase = initialValidation(symbol, shareAmount);
-
         int existingStock = stockHolds.getOrDefault(symbolUpperCase, 0);
 
         if (existingStock == 0) throw new StockNotAvailable("Stock not available in wallet.");
+        if (shareAmount > existingStock) throw new WrongShareAmount("Not enough shares to sell.");
 
-        if (existingStock - shareAmount > 0) {
-            stockHolds.put(symbolUpperCase, existingStock - shareAmount);
-        }
-        else if (existingStock - shareAmount == 0) {
+        if (shareAmount == existingStock) {
             stockHolds.remove(symbolUpperCase);
-        }
-        else {
-            throw new WrongShareAmount("Not enough shares to sell.");
+        } else {
+            stockHolds.put(symbolUpperCase, existingStock - shareAmount);
         }
         return true;
     }
 
-    public Integer checkStockCount(String symbol) {
+    public int checkStockCount(String symbol) {
         return stockHolds.getOrDefault(symbol.toUpperCase(), 0);
     }
 
-    public Integer checkUniqueStockSymbolCount() {
+    public int checkUniqueStockSymbolCount() {
         return stockHolds.size();
     }
 
@@ -54,14 +39,8 @@ public class UniqueStock {
     }
 
     private String initialValidation(String symbol, int shares) {
-        if (shares <= 0) {
-            throw new WrongShareAmount("Number of shares must be positive.");
-        }
-
-        if (!symbol.matches("[a-zA-Z]+")) {
-            throw new StockNotAvailable("Wrong symbol.");
-        }
-
+        if (shares <= 0) throw new WrongShareAmount("Number of shares must be positive.");
+        if (!symbol.matches("[a-zA-Z]+")) throw new StockNotAvailable("Wrong symbol.");
         return symbol.toUpperCase();
     }
 }
