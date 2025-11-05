a6f75d214cb0ab4cb0c502d77e3cacac7f1fc5e4
Andre Ferreira <anFerreira@infinera.com>
2025-11-05 14:03:02 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/exercises/stock/UniqueStockUnitTest.java

---
+++ src/main/java/com/langrsoft/exercises/stock/UniqueStock.java
@@ -0,0 +1,5 @@
+package com.langrsoft.exercises.stock;
+
+public class UniqueStock {
+
+}
---
+++ src/test/java/com/langrsoft/exercises/stock/UniqueStockUnitTest.java
@@ -0,0 +1,22 @@
+package com.langrsoft.exercises.stock;
+
+import org.junit.Test;
+
+public class UniqueStockUnitTest {
+    /*
+     zero stocks bought
+     One stock bought
+     Many stocks bought
+     buy same stock twice
+    * */
+
+    @Test
+    public void buyAStock(){
+
+    }
+
+    @Test
+    public void checkUniqueStock(){
+
+    }
+}
