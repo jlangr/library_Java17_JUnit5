af9157f4c15c7110c1ec83865b31f1d6511d3327
Gaurav <gaurav.1.agarwal@nokia.com>
2025-11-05 07:46:21 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/roman/ARomanNumberTest.java

--- src/test/java/com/langrsoft/roman/ARomanNumberTest.java
+++ src/test/java/com/langrsoft/roman/ARomanNumberTest.java
@@ -31,6 +31,11 @@ public class ARomanNumberTest {
     //500 -> D
     //1000 -> M
 
+    //Check repetion of nsymbols cases
+    //II
+    //VV
+    //XX
+
     //Check combinations of symbols
     //55 -> LV
     //15 -> XV
@@ -65,6 +70,52 @@ public class ARomanNumberTest {
             "1000, M"
     })
     public void testHardCodedRomanNumbers(int input, String expected) {
+        assert romanNumber.toRoman(input).equals(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, II",  //2 to 3 -> 1 to 3
+            "30, XXX", // 10 to 39 -> by 10 -> 1 to 3
+            "200, DD", //100 to 399 -> by 100 -> 1 to 3
+            "3000, MMM", //1000 to 8999 -> by 1000 -> 1 to 3
+    })
+    public void testHardCodedRomanNumbersUC2(int input, String expected) {
+        assert romanNumber.toRoman(input).equals(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, IV",  //2 to 3 -> 1 to 3
+            "9, IX",  //2 to 3 -> 1 to 3
+            "40, XL", // 10 to 39 -> by 10 -> 1 to 3
+            "90, XC", //100 to 399 -> by 100 -> 1 to 3
+            "400, CD", //1000 to 8999 -> by 1000 -> 1 to 3
+            "900, CM", //1000 to 8999 -> by 1000 -> 1 to 3
+    })
+    public void testHardCodedRomanNumbersUC3(int input, String expected) {
+        assert romanNumber.toRoman(input).equals(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "6, VI",  //2 to 3 -> 1 to 3
+            "60, LX", // 10 to 39 -> by 10 -> 1 to 3
+            "700, DCC", //100 to 399 -> by 100 -> 1 to 3
+            "1800, MDCCC", //1000 to 8999 -> by 1000 -> 1 to 3
+    })
+    public void testHardCodedRomanNumbersUC4(int input, String expected) {
+        assert romanNumber.toRoman(input).equals(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, VI",  //2 to 3 -> 1 to 3
+            "60, LX", // 10 to 39 -> by 10 -> 1 to 3
+            "700, DCC", //100 to 399 -> by 100 -> 1 to 3
+            "1800, MDCCC", //1000 to 8999 -> by 1000 -> 1 to 3
+    })
+    public void testHardCodedRomanNumbersUC5(int input, String expected) {
         assert romanNumber.toRoman(input).equals(expected);
     }
 
