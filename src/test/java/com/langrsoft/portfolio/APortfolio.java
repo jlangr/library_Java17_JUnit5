89aef598fbf54525d204e88b74e902a2758f9e2d
Gaurav <gaurav.1.agarwal@nokia.com>
2025-11-06 10:28:34 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/langrsoft/portfolio/APortfolio.java

--- src/test/java/com/langrsoft/portfolio/APortfolio.java
+++ src/test/java/com/langrsoft/portfolio/APortfolio.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class APortfolio {
 
     IStockPortFolioService mockStockService;
+    StockPortfolio port;
     @BeforeEach
     public void instantiate() {
         mockStockService = new IStockPortFolioService() {
@@ -21,105 +22,111 @@ public class APortfolio {
                 return 7;
             }
         };
+        port = new StockPortfolio(new IStockPortFolioService() {
+            @Override
+            public int getCurrentPrice(String stockSymbol) {
+                return 0;
+            }
+        });
     }
-//
-//    @Test
-//    public void testForZeroPurchase(){
-//        Assert.assertEquals(0, port.getUniqueCount());
-//    }
-//
-//    @Test
-//    public void testForSinglePurchase(){
-//        port.purchase("AAPL", 10);
-//        Assert.assertEquals(1, port.getUniqueCount());
-//
-//    }
-//
-//    @Test
-//    public void testForMultiplePurchaseOfDifferentSymbols(){
-//        port.purchase("AAPL", 10);
-//        port.purchase("GOOGL", 5);
-//        port.purchase("MSFT", 8);
-//        Assert.assertEquals(3, port.getUniqueCount());
-//
-//    }
-//
-//
-//    @Test
-//    public void testForMultiplePurchaseOfSimilarKindOfSymbols(){
-//        port.purchase("AAPL", 10);
-//        port.purchase("AAP", 5);
-//        Assert.assertEquals(2, port.getUniqueCount());
-//
-//    }
-//
-//    @Test
-//    public void testForMultiplePurchaseOfSameSymbol(){
-//        port.purchase("AAPL", 10);
-//        port.purchase("AAPL", 5);
-//        port.purchase("AAPL", 8);
-//        Assert.assertEquals(1, port.getUniqueCount());
-//
-//    }
-//
-//    //Z check for 0 value for symbol not purchased
-//    //O Purchase 1 share for a symbol and check if we get value 1
-//    //M Purchase multiple share with different number and check the value
-//    //M Purchase multiple time same share and check the value
-//    //B Purchase stock and add non valid value or 0. Check for exception
-//    //E validate the message in custom Exception
-//
-//    @Test
-//    public void tesForNoSharesPurchased(){
-//        Assert.assertEquals(0, port.getShareCount("AAPL"));
-//    }
-//
-//    @Test
-//    public void tesForOneSharePurchase(){
-//        port.purchase("TSLA", 1);
-//        Assert.assertEquals(1, port.getShareCount("TSLA"));
-//    }
-//
-//    @Test
-//    public void testForMultipleSharePurchase(){
-//        port.purchase("TSLA", 5);
-//        port.purchase("TSLA", 6);
-//        Assert.assertEquals(11, port.getShareCount("TSLA"));
-//    }
-//
-//    @Test
-//    public void testForMultipleSharesPurchase(){
-//        port.purchase("TSLA", 5);
-//        port.purchase("AAPL", 10);
-//        //port.purchase("X", 0);
-//        Assert.assertEquals(10, port.getShareCount("AAPL"));
-//        //Assert.assertEquals(5, port.getShareCount("TSLA"));
-//    }
-//
-//    @Test
-//    public void testForInvalidSharesCountPurchase(){
-//
-//
-//        var thrown = assertThrows(InvalidParameterException.class, () ->
-//                port.purchase("TSLA", 0));
-//        assertThat(thrown.getMessage())
-//                .isEqualTo("Purchase count should be more than 0");
-//
-//
-//    }
-//
-//
-//    @Test
-//    public void testForNegativeSharesCountPurchase(){
-//
-//
-//        var thrown = assertThrows(InvalidParameterException.class, () ->
-//                port.purchase("TSLA", -1));
-//        assertThat(thrown.getMessage())
-//                .isEqualTo("Purchase count should be more than 0");
-//
-//
-//    }
+
+    @Test
+    public void testForZeroPurchase(){
+        Assert.assertEquals(0, port.getUniqueCount());
+    }
+
+    @Test
+    public void testForSinglePurchase(){
+        port.purchase("AAPL", 10);
+        Assert.assertEquals(1, port.getUniqueCount());
+
+    }
+
+    @Test
+    public void testForMultiplePurchaseOfDifferentSymbols(){
+        port.purchase("AAPL", 10);
+        port.purchase("GOOGL", 5);
+        port.purchase("MSFT", 8);
+        Assert.assertEquals(3, port.getUniqueCount());
+
+    }
+
+
+    @Test
+    public void testForMultiplePurchaseOfSimilarKindOfSymbols(){
+        port.purchase("AAPL", 10);
+        port.purchase("AAP", 5);
+        Assert.assertEquals(2, port.getUniqueCount());
+
+    }
+
+    @Test
+    public void testForMultiplePurchaseOfSameSymbol(){
+        port.purchase("AAPL", 10);
+        port.purchase("AAPL", 5);
+        port.purchase("AAPL", 8);
+        Assert.assertEquals(1, port.getUniqueCount());
+
+    }
+
+    //Z check for 0 value for symbol not purchased
+    //O Purchase 1 share for a symbol and check if we get value 1
+    //M Purchase multiple share with different number and check the value
+    //M Purchase multiple time same share and check the value
+    //B Purchase stock and add non valid value or 0. Check for exception
+    //E validate the message in custom Exception
+
+    @Test
+    public void tesForNoSharesPurchased(){
+        Assert.assertEquals(0, port.getShareCount("AAPL"));
+    }
+
+    @Test
+    public void tesForOneSharePurchase(){
+        port.purchase("TSLA", 1);
+        Assert.assertEquals(1, port.getShareCount("TSLA"));
+    }
+
+    @Test
+    public void testForMultipleSharePurchase(){
+        port.purchase("TSLA", 5);
+        port.purchase("TSLA", 6);
+        Assert.assertEquals(11, port.getShareCount("TSLA"));
+    }
+
+    @Test
+    public void testForMultipleSharesPurchase(){
+        port.purchase("TSLA", 5);
+        port.purchase("AAPL", 10);
+        //port.purchase("X", 0);
+        Assert.assertEquals(10, port.getShareCount("AAPL"));
+        //Assert.assertEquals(5, port.getShareCount("TSLA"));
+    }
+
+    @Test
+    public void testForInvalidSharesCountPurchase(){
+
+
+        var thrown = assertThrows(InvalidParameterException.class, () ->
+                port.purchase("TSLA", 0));
+        assertThat(thrown.getMessage())
+                .isEqualTo("Purchase count should be more than 0");
+
+
+    }
+
+
+    @Test
+    public void testForNegativeSharesCountPurchase(){
+
+
+        var thrown = assertThrows(InvalidParameterException.class, () ->
+                port.purchase("TSLA", -1));
+        assertThat(thrown.getMessage())
+                .isEqualTo("Purchase count should be more than 0");
+
+
+    }
 
     @Test
     public void testPortFolioValueWhenNoShares() {
